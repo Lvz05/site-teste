74a6357835cd90e98d7c85010d7e8bf983b4b4c1
Lucas Alves <lucasalvesvasconcelos92@gmail.com>
2025-09-30 08:11:20 -0300

    aleterações nos codigos dos sites

--- funcoes.js
+++ funcoes.js
@@ -1,6 +1,8 @@
 function scrollToSection(id) {
-    const element = document.getElementById(id);
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
-    }
+  const element = document.getElementById(id);
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
   }
+}
+
+<script src="https://unpkg.com/feather-icons"></script>
